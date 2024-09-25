e597777750f8022b8e7864134aa17a4b1849b913
sreelesh <mail.sreelesh@gmail.com>
2024-09-25 22:03:30 +0530

    Email and Password Authentication (used useRef,Regex VAlidation)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NETFLIX_LOGO } from "../utils/constants";
 
 function Header() {
   return (
-    <div className="absolute px-32 py-6 z-50 bg-gradient-to-b w-full from-black">
+    <div className="absolute sm:px-32 px-4 py-4 sm:py-6 z-50 bg-gradient-to-b w-full from-black">
       <div>
         <img className="w-44 " src={NETFLIX_LOGO} alt="Logo" />
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,20 @@
 import { BACKGROUND_COLLAGE_IMG } from "../utils/constants";
 import Header from "./Header";
 import LoginForm from "./subComponents/LoginForm";
+import ResponsiveImage from "./subComponents/ResponsiveImage";
 
 const Login = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black ">
       <Header />
       <LoginForm />
       <div className="">
-        <img
-          className="h-screen w-full"
+        <ResponsiveImage src={BACKGROUND_COLLAGE_IMG} alt="BG_IMG" />
+        {/* <img
+          className="h-screen   w-full"
           src={BACKGROUND_COLLAGE_IMG}
           alt="BG_IMG"
-        />
+        /> */}
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
     </div>
